2ad793e795f9bcc32a26578f1b76877b196df0b6
ELIZA <rysaeva96@mail.ru>
2022-05-26 13:38:48 +0500

    Решение 29 задачи

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -3,4 +3,23 @@
 // Размер массива определяется пользователем.
 
 
+void FillArray()
+{
+    Console.WriteLine("Введите число элементов массива: ");
+    int N = int.Parse(Console.ReadLine());
+    int[] arr = new int[N];
+    Random rand = new Random();
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = rand.Next(0, 100);
+        Console.Write(arr[i] + ", ");
+    }
+    return;
+}
+FillArray();
+Console.WriteLine();
+
+
+
+
 
