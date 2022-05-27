17dd61aa3153eef09824ff00c9fb0871d8dd1bc8
Элиза Рысаева <103875177+reistorm@users.noreply.github.com>
2022-05-27 23:19:06 +0500

    Update Program.cs
    Проверить позже

--- Task_3/Program.cs
+++ Task_3/Program.cs
@@ -20,6 +20,20 @@ void FillArray()
 FillArray();
 Console.WriteLine();
 
+//int[] initArray []
+{
+    Console.WriteLine("Введите число элементов массива: );
+    int N = int.Parse(Console.ReadLine());
+    int [] arr = new int[N];
+    Console.Write("[ ");
+    for (int i = 0; i < arr.Lenght; i++)
+    { 
+       arr[i] = rand.Next(0, 100);
+       Console.Write(arr[i] + ", ");
+       Console.Write("] ");
+    } 
+    return arr;
+}
 
 
 
