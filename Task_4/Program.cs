ae45c03ce3f59a65001225255a91384c40edc2cb
ELIZA <rysaeva96@mail.ru>
2022-05-27 18:16:15 +0500

    Недоработанный вариант доп.задачи

---
+++ Task_4/Program.cs
@@ -0,0 +1,31 @@
+﻿// Напишите функцию, которая принимает слово 
+// и возвращает true, если в этом слове есть 
+//две одинаковые, стоящие рядом буквы.
+//"ссора" -> true
+//"клюющий" -> true
+//"кодер" -> false
+//"цыпленок"-> false
+
+
+Console.WriteLine("Введите слово = ");
+string userString = Console.ReadLine();
+Console.WriteLine(userString);
+int Length = userString.Length;
+for (int i = 0; i < Length; i++)
+{
+    bool isTwoSideYes = true;
+    bool isTwoSideNo = false;
+    if (userString[i] == userString[i+1])
+    {
+        Console.WriteLine(isTwoSideYes);
+        break;  
+        
+    }
+    else 
+    {
+        Console.WriteLine(isTwoSideNo);
+
+    }
+}
+
+
