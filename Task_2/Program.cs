aa86d56a628b392630c69c17afc6f8521b8c488a
ELIZA <rysaeva96@mail.ru>
2022-05-27 16:34:12 +0500

    Решение 27 задачи

---
+++ Task_2/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе. 
+//Задачу решить без использования строк (string).
+
+
+    Console.WriteLine("Введите Натуральное число = ");
+    int num = int.Parse(Console.ReadLine());
+    int dig = 0;
+    for (int i = 1; i < num; i = i * 10)
+    {
+        dig++;
+    }
+    Console.WriteLine($"Количество цифр в строке {num} = {dig}");
+
+    int sum = 0;
+    for (int i = 0; i < dig; i++)
+    {
+        int num1 = num % 10;
+        num = num / 10;
+        sum = num1 + sum;
+    }
+Console.WriteLine(sum);
+
+
+
+
